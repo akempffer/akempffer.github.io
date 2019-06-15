6d329062048aef141744822b8ced19321cacefa8
akempffer <46541728+akempffer@users.noreply.github.com>
2019-06-14 17:03:48 -0700

    Created Stylings for index.html

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,13 @@ $('nav').tabslet({
   
     mouseevent: 'hover'
     
-    
   });
+  
+  const greetings = ['Artistic', 'Eclectic', 'Sweet', 'Energetic', 'Inspired', 'Friendly'];
+  console.log( greetings[4] );
+  
+  let max = 6;
+  let randNum = Math.floor( Math.random() * max);
+  console.log( randNum );
+  
+  $('h2').text( greetings[randNum ] );
